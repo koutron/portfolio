caf4ac1471760ee29f78df2572c6c8f45eb40911
koutron <kamousavi@gmail.com>
2017-11-17 19:11:16 -0500

    added about and input sections

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		size: 200, // Font size, defined by the height of the letters (pixels)
 		weight: 3, // Font weight (pixels)
 		rounded: true, // Rounded letter endings
-		color: '#b8f8d4', // Font color '#b8f8d4'
+		color: '#8BECB5', // Font color '#b8f8d4'
 		duration: 1, // Duration of the animation of each letter (seconds)
 		delay: [0, 0.1], // Delay animation among letters (seconds)
 		fade: 1, // Fade effect duration (seconds)
@@ -18,4 +18,57 @@ $(document).ready(function() {
 	$('.fancy-name').mouseenter(function() {
 		myText.show(options);
 	});
+
+	let reset = new Waypoint({
+		element: document.getElementById('img-face'),
+		handler: function() {
+			$('.nav-menu>li>a').css('color', 'white');
+		}
+	});
+
+	let projectsWp = new Waypoint({
+	  element: document.getElementById('projects'),
+	  handler: function() {
+			$('.nav-menu>li>a').css('color', 'white');
+	    $('#wp-projects').css('color', '#408DB3');
+	  },
+		offset: 90
+	});
+
+	let projectsWpEnd = new Waypoint({
+		element: document.getElementById('freecodecamp'),
+		handler: function() {
+			$('.nav-menu>li>a').css('color', 'white');
+			$('#wp-projects').css('color', '#408DB3');
+		},
+		offset: 90
+	});
+
+	var resumeWp = new Waypoint({
+		element: document.getElementById('resume'),
+		handler: function() {
+			$('.nav-menu>li>a').css('color', 'white');
+			$('#wp-resume').css('color', '#408DB3');
+		}
+	});
+
+	var resumeWpEnd = new Waypoint({
+		element: document.getElementById('skills'),
+		handler: function() {
+			$('.nav-menu>li>a').css('color', 'white');
+			$('#wp-resume').css('color', '#408DB3');
+		}
+	});
+
+	var aboutWp = new Waypoint({
+		element: document.getElementById('about'),
+		handler: function() {
+			$('.nav-menu>li>a').css('color', 'white');
+			$('#wp-about').css('color', '#408DB3');
+		}
+	});
+
+
+
+
 });
